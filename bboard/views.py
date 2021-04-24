26b73afb6154d214ca380c1b3136054031e23406
Bekzod <bekzod030900@gmail.com>
2021-04-24 15:38:56 +0500

    views

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bb
 
 def index(request):
-    return HttpResponse('<h1>Django Project</h1>')
+    s = 'Reklamalar ro\'yhati\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type = 'text/plain; charset = utf-8')
