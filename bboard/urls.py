c773eb4b6e00b21e8969a9651d3f7acb5837f771
bekzod818 <bekzod030900@gmail.com>
2021-05-20 19:22:56 +0500

    createView

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('', index, name = 'home'),
-    path('<int:rubric_id>/', by_rubric),
+    path('', index, name = 'index'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add/', BbCreateView.as_view(), name='add'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Bb, Rubric
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+from django.urls import reverse_lazy
 
 def index(request):
     bbs = Bb.objects.all()
@@ -24,3 +27,13 @@ def by_rubric(request, rubric_id):
         'current_rubric': current_rubric,
     }
     return render(request, 'bboard/by_rubric.html', context)    
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
