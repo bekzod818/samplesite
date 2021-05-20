47321df2f5723f83f1ab8a2c708ecb103976a1b2
bekzod818 <bekzod030900@gmail.com>
2021-05-20 23:44:37 +0500

    jinja va css

--- bboard/forms.py
+++ bboard/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 from .models import Bb
 
 class BbForm(ModelForm):
     class Meta:
         model = Bb
         fields = ('title', 'content', 'price', 'rubric')
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-input'}),
+            'content': Textarea(attrs={'cols': 60, 'rows': 10})
+        }
 
